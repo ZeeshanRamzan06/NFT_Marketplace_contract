b4f391dabec4c1952466d6f620698f34e0ba105b
ZeeshanRamzan06 <zeeshanramzan3f@gmail.com>
2025-01-21 17:28:45 +0500

    update

--- basicminting.sol
+++ basicminting.sol
@@ -1,34 +1,45 @@
 // SPDX-License-Identifier: MIT
-    pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-
-    contract MintingContract{
+contract NFTContract {
+    address public owner;
+    uint256 public totalSupply;
+    uint256 private lastTokenId; 
 
     struct Nft {
+        uint256 tokenId;
         string name;
         string symbol;
         uint256 price;
     }
 
-    mapping(uint256 => Nft) public Nfts; 
-    mapping(uint256 => address) public tokenOwners;
-    
-    address public owner;
-    uint256 public totalSupply;
+    mapping(address => Nft[]) public ownerToNFTs; 
+    mapping(uint256 => address) public tokenOwners; 
 
     constructor() {
         owner = msg.sender;
-        totalSupply = 0;
+        lastTokenId = 0; 
     }
 
-    
-    function mintNft(uint256 _tokenid,string memory _name,string memory _symbol,uint256 _price) public {
+    function mintNft(string memory _name, string memory _symbol, uint256 _price) public {
         require(msg.sender == owner, "Only owner can mint NFTs");
-        require(tokenOwners[_tokenid] == address(0), "Token ID already exists");
-        Nfts[_tokenid] = Nft(_name, _symbol, _price);
-        tokenOwners[_tokenid] = owner;
-        totalSupply += 1;
+
+        uint256 newTokenId = ++lastTokenId;
+
+        Nft memory newNft = Nft({
+            tokenId: newTokenId,
+            name: _name,
+            symbol: _symbol,
+            price: _price
+        });
+
+        ownerToNFTs[owner].push(newNft);
+        tokenOwners[newTokenId] = owner;
+
+        totalSupply++;
     }
 
-
+    function getNFTsByOwner(address _owner) public view returns (Nft[] memory) {
+        return ownerToNFTs[_owner];
+    }
 }
